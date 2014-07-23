9eca8aaefe558e8b6fb7c7144bebaed14f840503
Yukineko <yukineko.mew7@gmail.com>
2014-07-24 03:05:54 +0900

    実装してみた第二弾

--- Fouriertransform.java
+++ Fouriertransform.java
@@ -16,57 +16,71 @@ package FT;
 
 public class Fouriertransform {
 	
-	private final int MAX_DATA;
-	private int [] coef = {0, 1, 2, 8, 16};	
-	private double [] dataset;
+	private final int MAX_DATA;	
 	
 	private double [] real;
 	private double [] imaginary;
 	
 	public Fouriertransform(int maxData) {
 		MAX_DATA = maxData;
-		dataset = new double[MAX_DATA];
 		real = new double[MAX_DATA];
 		imaginary = new double[MAX_DATA];
-		makeDataset();
-		dftCalc();
 	}
 	
-	private void makeDataset() {
-//		for(int i=0; i<coef.length; i++) {
-//			for(int j=0; j<MAX_DATA; j++) {
-//				dataset[i] = Math.cos( coef[i]*(2*Math.PI)*(j/(double)MAX_DATA) );
-//			}
-//		}
+	public void testFft() {
+//		double [] testData = new double[MAX_DATA];
+//		double [] empty = new double[MAX_DATA];
 		for(int i=0; i<MAX_DATA; i++) {
-			dataset[i] = Math.random();
+			imaginary[i] = 0.0;
+			real[i] = Math.random();
 		}
+		fftCalc(real, imaginary);
 	}
 	
-	private void dftCalc() {
-		if( MAX_DATA <= 1 ) return ;
-		for(int i=0; i<MAX_DATA; ++i) {
-			double realSum = 0;
-			double imaginarySum = 0;
-			for(int j=0; j<MAX_DATA; ++j ) {
-				double theta = 2*Math.PI/(double)MAX_DATA*i*j;
-				realSum = dataset[j]*Math.cos(theta);
-				imaginarySum = dataset[j]*Math.sin(theta);
-			}
-			real[i] = realSum;
-//			System.out.println(realSum);
-			imaginary[i] = imaginarySum;
+	private void fftCalc(double [] theReal, double [] theImaginary) {
+		int num = MAX_DATA;
+		double theta = -2*Math.PI/num;
+		fft(num, theta, theReal, theImaginary);
+	}
+	
+	private void fft(int num, double theta, double [] theReal, double [] theImaginary) {
+		if( num <= 1 ) return ;
+		
+		int halfNum = num/2;
+		double [] realOdd = new double[halfNum];
+		double [] imaginaryOdd = new double[halfNum];
+		double [] realEven = new double[halfNum];
+		double [] imaginaryEven = new double[halfNum];
+		
+		for( int m=0; m<halfNum; m++) {
+			realEven[m] = theReal[m] + theReal[halfNum+m];
+			imaginaryEven[m] =  theImaginary[m] + theImaginary[halfNum+m];
+			double Rm = theReal[m] - theReal[halfNum+m];
+			double Im = theImaginary[m] - theImaginary[halfNum+m];
+			double cos = Math.cos(m*theta);
+			double sin = Math.sin(m*theta);
+			
+			realOdd[m] = Rm*cos - Im*sin;
+			imaginaryOdd[m] = Rm*sin + Im*cos;
+		}
+		
+		fft(halfNum, 2*theta, realEven, imaginaryEven);
+		fft(halfNum, 2*theta, realOdd, imaginaryOdd);
+		
+		
+		
+		for(int i=0; i<halfNum; i++) {
+			theReal[2*i] = realEven[i];
+			theReal[2*i+1] = realOdd[i];
+			theImaginary[2*i] = imaginaryEven[i];
+			theImaginary[2*i+1] = imaginaryOdd[i];
 		}
 	}
 	
 	public void show() {
 		for(int i=0; i<MAX_DATA; i++) {
-//			System.out.println("dataset = [" + dataset[i] +"], real = [" + real[i] + "], imaginary = [" + imaginary[i] + "]");
+			System.out.println("real = [" + real[i] + "], imaginary = [" + imaginary[i] + "]");
 		}
-	}
-	
-	public double [] getDatset() {
-		return dataset;
 	}
 	
 	public double [] getReal() {
@@ -75,12 +89,5 @@ public class Fouriertransform {
 	
 	public double [] getImaginary() {
 		return imaginary;
-	}
-	
-	public static void main(String [] args) {
-		Fouriertransform ft = new Fouriertransform(32);
-		ft.makeDataset();
-		ft.dftCalc();
-		ft.show();
 	}
 }
